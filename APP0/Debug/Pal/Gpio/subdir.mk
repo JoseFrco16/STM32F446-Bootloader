3796ad8d57ea31ee596162778e2ae143940aafd4
Jose Francisco <josem98201016@gmail.com>
2026-04-06 12:35:39 +0200

    fix: Makefile build

--- APP0/Debug/Pal/Gpio/subdir.mk
+++ APP0/Debug/Pal/Gpio/subdir.mk
@@ -15,7 +15,7 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Pal/Gpio/%.o Pal/Gpio/%.su: ../Pal/Gpio/%.c Pal/Gpio/subdir.mk
+Pal/Gpio/%.o: ../Pal/Gpio/%.c Pal/Gpio/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Hal/Stm32_Drivers/STM32F4xx_HAL_Driver/Inc -I../Hal/Stm32_Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Hal/Stm32_Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Hal/Stm32_Drivers/CMSIS/Include -I"../Pal/Uart" -I"../Pal/Timer" -I"../App/Main" -I"../App/Boot" -I"../Pal/Gpio" -I"../Pal/Pal_Mngr" -I"../Pal/Mcu" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Pal-2f-Gpio
--- Bootloader/Debug/Pal/Mcu/subdir.mk
+++ Bootloader/Debug/Pal/Mcu/subdir.mk
@@ -18,7 +18,7 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Pal/Mcu/%.o Pal/Mcu/%.su: ../Pal/Mcu/%.c Pal/Mcu/subdir.mk
+Pal/Mcu/%.o: ../Pal/Mcu/%.c Pal/Mcu/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Hal/Stm32_Drivers/STM32F4xx_HAL_Driver/Inc -I../Hal/Stm32_Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Hal/Stm32_Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Hal/Stm32_Drivers/CMSIS/Include -I"../Pal/Uart" -I"../App/Header" -I"../App/Main" -I"../App/Verif" -I"../App/Boot" -I"../Pal/Gpio" -I"../Pal/Pal_Mngr" -I"../Pal/Mcu" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Pal-2f-Mcu
