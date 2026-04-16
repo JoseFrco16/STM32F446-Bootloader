e27a6a269a0dd4a8cf4f7fa6b7e8a2a9b4a48f2c
José Francisco Martí Martín <josem98201016@gmail.com>
2026-04-16 10:43:41 +0100

    fix: Update flash subdir.mk
    Parent folder updated

--- APP0/Debug/Pal/Flash/subdir.mk
+++ APP0/Debug/Pal/Flash/subdir.mk
@@ -5,23 +5,23 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../App/Flash/Flash.c 
+../Pal/Flash/Flash.c 
 
 OBJS += \
-./App/Flash/Flash.o 
+./Pal/Flash/Flash.o 
 
 C_DEPS += \
-./App/Flash/Flash.d 
+./Pal/Flash/Flash.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-App/Flash/%.o: ../App/Flash/%.c App/Flash/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Hal/Stm32_Drivers/STM32F4xx_HAL_Driver/Inc -I../Hal/Stm32_Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Hal/Stm32_Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Hal/Stm32_Drivers/CMSIS/Include -I"../Pal/Uart" -I"../Pal/Timer" -I"../App/Main" -I"../App/Flash" -I"../Pal/Gpio" -I"../Pal/Pal_Mngr" -I"../Pal/Mcu" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Pal/Flash/%.o: ../Pal/Flash/%.c Pal/Flash/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Hal/Stm32_Drivers/STM32F4xx_HAL_Driver/Inc -I../Hal/Stm32_Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Hal/Stm32_Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Hal/Stm32_Drivers/CMSIS/Include -I"../Pal/Uart" -I"../Pal/Timer" -I"../Pal/Main" -I"../Pal/Flash" -I"../Pal/Gpio" -I"../Pal/Pal_Mngr" -I"../Pal/Mcu" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
-clean: clean-App-2f-Flash
+clean: clean-Pal-2f-Flash
 
-clean-App-2f-Flash:
-	-$(RM) ./App/Flash/Flash.d ./App/Flash/Flash.o ./App/Flash/Flash.su
+clean-Pal-2f-Flash:
+	-$(RM) ./Pal/Flash/Flash.d ./Pal/Flash/Flash.o ./Pal/Flash/Flash.su
 
-.PHONY: clean-App-2f-Flash
+.PHONY: clean-Pal-2f-Flash
 
